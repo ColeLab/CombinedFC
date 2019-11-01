3552072b2e2e45df81a01f06adff785608865957
rubenSaro <40474901+rubenSaro@users.noreply.github.com>
2019-11-01 15:57:32 -0400

    Add files via upload

---
+++ CombinedFCToolBox/MidpointNormalize.py
@@ -0,0 +1,19 @@
+import matplotlib.colors as colors
+import numpy as np
+
+class MidpointNormalize(colors.Normalize):
+    """
+    Normalise the colorbar so that diverging bars work there way either side from a prescribed midpoint value)
+
+    e.g. im=ax1.imshow(array, norm=MidpointNormalize(midpoint=0.,vmin=-100, vmax=100))
+    """
+    def __init__(self, vmin=None, vmax=None, midpoint=None, clip=False):
+        self.midpoint = midpoint
+        colors.Normalize.__init__(self, vmin, vmax, clip)
+
+    def __call__(self, value, clip=None):
+        
+        # I'm ignoring masked values and all kinds of edge cases to make a
+        # simple example...
+        x, y = [self.vmin, self.midpoint, self.vmax], [0, 0.5, 1]
+        return np.ma.masked_array(np.interp(value, x, y), np.isnan(value))
