d40b6ec93dcb229a120dd813114331d1deacecb2
rubenSaro <40474901+rubenSaro@users.noreply.github.com>
2019-12-03 17:05:18 -0500

    Add files via upload
    include the MidpointNormalize class in the function

--- CombinedFCToolBox/plotConnectivityMatrix.py
+++ CombinedFCToolBox/plotConnectivityMatrix.py
@@ -7,14 +7,15 @@
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 from mpl_toolkits.axes_grid1 import make_axes_locatable
-from MidpointNormalize import MidpointNormalize
 import numpy as np
+import matplotlib.colors as colors
 
 def plotConnectivityMatrix(ConnMat,
                            methodTitle='Correlation',
+                           functionalNetworks = False,
                            networkColorBar_x = True,
                            networkColorBar_y = True,
-                           networkLabels = False):  
+                           networkLabels = True):  
     '''
     INPUT: 
         ConnMat : weighted connectivity matrix
@@ -32,12 +33,18 @@ def plotConnectivityMatrix(ConnMat,
                 fig.show()
     '''
     
-    #Glasser 360 cortex parcellation ordered into functional networks reported in Ji et al., (2019)
-    #make as integer and subtract 1, so the indices start in 0, as Python requires.
-    #path where the network file is: it contains labels (first column) and order (second column)
-    netFile = np.loadtxt('CombinedFCToolBox/aux_files/networks_labels.txt',delimiter=',')
-    #to assign each of the 360 nodes to its corresponding network
-    netOrder = netFile[:,1].astype(int) - 1 
+    if functionalNetworks == False:
+        netOrder = np.arange(ConnMat.shape[0])
+    
+    #To apply the functional network order from Ji et al., (2019)
+    elif functionalNetworks == True:
+        #Glasser 360 cortex parcellation ordered into functional networks reported in Ji et al., (2019)
+        #make as integer and subtract 1, so the indices start in 0, as Python requires.
+        #path where the network file is: it contains labels (first column) and order (second column)
+        netFile = np.loadtxt('CombinedFCToolBox/aux_files/networks_labels.txt',delimiter=',')
+        #to assign each of the 360 nodes to its corresponding network
+        netOrder = netFile[:,1].astype(int) - 1 
+
     
     #for the weights colorbar: red positive, blue negative
     v_min = np.min(ConnMat)
@@ -53,8 +60,10 @@ def plotConnectivityMatrix(ConnMat,
                     norm=MidpointNormalize(midpoint=v_mid,vmin=v_min, vmax=v_max))
     plt.title(methodTitle,fontsize=16)
     #axes ticks labels
-    plt.xticks([1,180,360])
-    plt.yticks([1,180,360])
+    a = np.round(ConnMat.shape[0]/2).astype(int)
+    b = np.round(ConnMat.shape[0])
+    plt.xticks([1,a,b])
+    plt.yticks([1,a,b])
     #labels font size and length of the ticks
     #ax.tick_params(labelsize=14,length=0.01)
     #Thickness of the connectivity matrix border
@@ -67,37 +76,38 @@ def plotConnectivityMatrix(ConnMat,
     #cbar.set_ticks([])
     
     
-    #To create the networks colorbars
-    #network palette defined by the Cole lab:
-    networkPalette = ['royalblue','slateblue','paleturquoise','darkorchid','limegreen',
-                      'lightseagreen','yellow','orchid','r','peru','orange','olivedrab']
-    #in case the networks labels want to be used in the plot
-    orderedNetworks = ['VIS1','VIS2','SMN','CON','DAN','LAN','FPN','AUD','DMN','PMM','VMM','ORA']
-    #define the colormap as an independent graphical object
-    cmap = mpl.colors.ListedColormap(networkPalette)
-    #number of nodes (size) in each of the 12 networks
-    size_networks=[]
-    netLabels = netFile[:,0].astype(int)
-    #loop through all the labels: 1 to 12 to count the number of nodes
-    for i in range(np.max(netLabels)):
-        size_networks.append(np.sum(netLabels==i+1))
-    #the bounds of the bar are the cumulative sum for each network size starting at zero: 
-    #ie, 0, 6, 6+54, 6+54+39, etc...
-    su = 0
-    #the first element of the networks bar bounds is zero
-    bounds = [0]
-    #this loop makes the cumulative sums
-    for i in range(np.max(netLabels)):
-        su += size_networks[i]
-        bounds.append(su)
-    #define the size of the color blocks according to the bounds (ie. the number of nodes)
-    norm = mpl.colors.BoundaryNorm(bounds, cmap.N)
-      
-    #get the size and position of the connectivity matrix as a reference 
-    #to position the networks color bars
-    l, b, w, h = ax.get_position().bounds
+    if functionalNetworks == True:
+        #To create the networks colorbars
+        #network palette defined by the Cole lab:
+        networkPalette = ['royalblue','slateblue','paleturquoise','darkorchid','limegreen',
+                          'lightseagreen','yellow','orchid','r','peru','orange','olivedrab']
+        #in case the networks labels want to be used in the plot
+        orderedNetworks = ['VIS1','VIS2','SMN','CON','DAN','LAN','FPN','AUD','DMN','PMM','VMM','ORA']
+        #define the colormap as an independent graphical object
+        cmap = mpl.colors.ListedColormap(networkPalette)
+        #number of nodes (size) in each of the 12 networks
+        size_networks=[]
+        netLabels = netFile[:,0].astype(int)
+        #loop through all the labels: 1 to 12 to count the number of nodes
+        for i in range(np.max(netLabels)):
+            size_networks.append(np.sum(netLabels==i+1))
+        #the bounds of the bar are the cumulative sum for each network size starting at zero: 
+        #ie, 0, 6, 6+54, 6+54+39, etc...
+        su = 0
+        #the first element of the networks bar bounds is zero
+        bounds = [0]
+        #this loop makes the cumulative sums
+        for i in range(np.max(netLabels)):
+            su += size_networks[i]
+            bounds.append(su)
+        #define the size of the color blocks according to the bounds (ie. the number of nodes)
+        norm = mpl.colors.BoundaryNorm(bounds, cmap.N)
+
+        #get the size and position of the connectivity matrix as a reference 
+        #to position the networks color bars
+        l, b, w, h = ax.get_position().bounds
     
-    if networkColorBar_x == True:
+    if networkColorBar_x == True and functionalNetworks == True:
         ax1 = fig.add_axes([l,b/1.4,w,h/25])
         cbNet = mpl.colorbar.ColorbarBase(ax1, cmap=cmap,
                                         norm=norm,
@@ -109,7 +119,7 @@ def plotConnectivityMatrix(ConnMat,
         cbNet.set_ticks([])
     
     
-    if networkColorBar_y == True:
+    if networkColorBar_y == True and functionalNetworks == True:
         ax2 = fig.add_axes([l/1.105,b,w/25,h])
 
         cbNet = mpl.colorbar.ColorbarBase(ax2, cmap=cmap,
@@ -120,18 +130,22 @@ def plotConnectivityMatrix(ConnMat,
         cbNet.set_ticks([])
     
     
-    #make extra adjustment to the position of the matrix labels if networks colorbars are used
-    if networkColorBar_x == True:
+    #make extra adjustment to the position of the matrix labels if functional networks colorbars are used
+    if networkColorBar_x == True and functionalNetworks == True:
         ax.tick_params(axis='x',labelsize=14,pad=18,length=0.01)
     elif networkColorBar_x != True:
         ax.tick_params(axis='x',labelsize=14,length=0.01)
 
-    if networkColorBar_y == True:
+    if networkColorBar_y == True and functionalNetworks == True:
         ax.tick_params(axis='y',labelsize=14,pad=18,length=0.01)
     elif networkColorBar_y != True:
         ax.tick_params(axis='y',labelsize=14,length=0.01)
         
-    if networkLabels == True:
+    if functionalNetworks == False:
+        ax.tick_params(axis='y',labelsize=14,length=0.01)
+        ax.tick_params(axis='x',labelsize=14,length=0.01)
+        
+    if networkLabels == True and functionalNetworks == True:
         #in case the networks labels are used in the plot
         orderedNetworks = ['VIS1','VIS2','SMN','CON','DAN','LAN','FPN','AUD','DMN','PMM','VMM','ORA']
         #remove the number labels
@@ -161,3 +175,21 @@ def plotConnectivityMatrix(ConnMat,
     #    ax2.invert_xaxis()
     
     return fig,ax
+
+
+class MidpointNormalize(colors.Normalize):
+    """
+    Normalise the colorbar so that diverging bars work there way either side from a prescribed midpoint value)
+
+    e.g. im=ax1.imshow(array, norm=MidpointNormalize(midpoint=0.,vmin=-100, vmax=100))
+    """
+    def __init__(self, vmin=None, vmax=None, midpoint=None, clip=False):
+        self.midpoint = midpoint
+        colors.Normalize.__init__(self, vmin, vmax, clip)
+
+    def __call__(self, value, clip=None):
+        
+        # I'm ignoring masked values and all kinds of edge cases to make a
+        # simple example...
+        x, y = [self.vmin, self.midpoint, self.vmax], [0, 0.5, 1]
+        return np.ma.masked_array(np.interp(value, x, y), np.isnan(value))
